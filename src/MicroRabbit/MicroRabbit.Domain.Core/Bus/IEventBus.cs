02a7538cc46572fbf9ab6d8ea8214782940e058a
Eduardo Flores <efloresm@minsait.com>
2023-03-21 23:27:24 +0100

    Implemented Interfaces and classes for MicroRabbit.Domain.Core

---
+++ src/MicroRabbit/MicroRabbit.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,12 @@
+﻿using MicroRabbit.Domain.Core.Commands;
+using MicroRabbit.Domain.Core.Events;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public  interface IEventBus
+    {
+        Task SendCommand<TCommand>(TCommand command) where TCommand : Command;
+        void Publish<TEvent>(TEvent @event) where TEvent : Event;
+        void Subscribe<TEvent, TEventHandler>() where TEvent : Event where TEventHandler : IEventHandler<TEvent>;
+    }
+}
---
+++ src/MicroRabbit/MicroRabbit.Domain.Core/Events/Event.cs
@@ -0,0 +1,13 @@
+﻿
+namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Event
+    {
+        protected Event()
+        {
+            Timestamp = DateTime.UtcNow;
+        }
+
+        public DateTime Timestamp { get; protected set; }
+    }
+}
